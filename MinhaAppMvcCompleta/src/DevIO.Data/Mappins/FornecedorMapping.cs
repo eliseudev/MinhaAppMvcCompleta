8aedfd25d653614c99ddb30d934c4a74777217e8
Eliseu Oliveira <eliseu.dev@outlook.com>
2021-03-29 14:37:27 -0300

    .NetCore
    Modelagem do banco de dados e criação dos scripts para criar tabela.

---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Mappins/FornecedorMapping.cs
@@ -0,0 +1,35 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappins
+{
+    /* Modelagem da Tabela Fornecedor */
+    class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            /*PrimaryKey*/
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Documento)
+               .IsRequired()
+               .HasColumnType("varchar(14)");
+
+            /* Relacionamento de 1 : 1 => Fornecedor : Endereco */
+            builder.HasOne(f => f.Endereco)
+                .WithOne(f => f.Fornecedor);
+
+            /* Relacionamento de 1 : N => Fornecedor : Produtos */
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}

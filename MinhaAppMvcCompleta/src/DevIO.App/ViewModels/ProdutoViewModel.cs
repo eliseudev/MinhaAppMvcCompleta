f49dcbe93433fc3b7c432f2b67eed95cf1e82df4
eliseudev <eliseu.dev@outlook.com>
2021-04-11 01:14:15 -0300

    Implementação de Designer e Controller

--- MinhaAppMvcCompleta/src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -13,6 +13,10 @@ namespace DevIO.App.ViewModels
         [Key]
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage ="O Campos {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
@@ -22,7 +26,7 @@ namespace DevIO.App.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        public IFormFile ImagemUpload { get; set; }
+        //public IFormFile ImagemUpload { get; set; }
 
         public string Imagem { get; set; }
 
@@ -37,5 +41,7 @@ namespace DevIO.App.ViewModels
 
         /* EF Relations */
         public ProdutoViewModel Fornecedor { get; set; }
+
+        public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
     }
 }
